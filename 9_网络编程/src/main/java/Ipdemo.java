9fe5eee3d12a2148a8cf1115a68390dae5648b24
Aurora <2159338806@qq.com>
2022-10-26 09:09:42 +0800

    update

--- 9_网络编程/src/main/java/Ipdemo.java
+++ 9_网络编程/src/main/java/Ipdemo.java
@@ -5,13 +5,19 @@ import java.net.UnknownHostException;
  * @author Liu
  * @create 2022-10-25-11:37
  * @description:
+ * IP:唯一的标识 Internet上的计算机(通信实体)
+ * 在java中使用InetAddress类代表ip
  */
 public class Ipdemo {
     public static void main(String[] args) throws UnknownHostException {
         //
         InetAddress localHost=InetAddress.getLocalHost();
+        System.out.println(localHost);
         //获得主机IP
         String hostAddress = localHost.getHostAddress();
         System.out.println(hostAddress);
+
+        InetAddress byName = InetAddress.getByName("www.vip.com");
+        System.out.println(byName);
     }
 }
